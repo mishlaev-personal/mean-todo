be7943ee87e25ce308da1a7e44859f5c85c86cef
Vitaliy Myshlyaev <vit@concertwith.me>
2016-09-29 01:59:12 +0300

    repacked todo.bundle.js with webpack

--- public/scripts/todo.bundle.js
+++ public/scripts/todo.bundle.js
@@ -8,8 +8,10 @@ webpackJsonp([0],[
 
 	angular.module('todoListApp', []);
 
-
 	__webpack_require__(3);
+	__webpack_require__(5);
+	__webpack_require__(7);
+
 
 /***/ },
 /* 1 */,
@@ -21,20 +23,123 @@ webpackJsonp([0],[
 
 	var angular = __webpack_require__(1);
 
-	angular.module('todoListApp')
-	.controller('mainCtrl', function($scope, dataService){
-	  
+	angular.module('todoListApp').service('dataService', __webpack_require__(4));
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	function DataService ($http) {
+
+	  this.getTodos = function(cb) {
+	    $http.get('/api/todos').then(cb);
+	  };
+
+	  this.deleteTodo = function(todo) {
+	    console.log("I deleted the " + todo.name + " todo!");
+	  };
+
+	  this.saveTodos = function(todos) {
+	    console.log("I saved " + todos.length + " todos!");
+	  };
+
+	}
+
+	module.exports = DataService;
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var angular = __webpack_require__(1);
+
+	angular.module('todoListApp').directive('todo', __webpack_require__(6));
+
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	function ToDoDirective () {
+	  return {
+	    templateUrl: 'templates/todo.html',
+	    replace: true,
+	    controller: 'todoCtrl'
+	  }
+	}
+
+	module.exports = ToDoDirective;
+
+
+/***/ },
+/* 7 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var angular = __webpack_require__(1);
+
+	angular.module('todoListApp').controller('mainCtrl', __webpack_require__(8));
+	angular.module('todoListApp').controller('todoCtrl', __webpack_require__(9));
+
+
+/***/ },
+/* 8 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	function MainCtrl ($scope, dataService) {
+
 	  dataService.getTodos(function(response){
-	    var todos = response.data;  
+	    var todos = response.data.todos;
 	    $scope.todos =  todos;
-	    });
-	  
+	  });
+
 	  $scope.addTodo = function() {
 	    $scope.todos.unshift({name: "This is a new todo.",
 	                      completed: false});
 	  };
-	  
-	})
+
+	}
+
+	module.exports = MainCtrl;
+
+
+/***/ },
+/* 9 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	function TodoCtrl ($scope, dataService) {
+
+	  $scope.deleteTodo = function(todo, index) {
+	    $scope.todos.splice(index, 1);
+	    dataService.deleteTodo(todo);
+	  };
+
+	  $scope.saveTodos = function() {
+	    var filteredTodos = $scope.todos.filter(function(todo){
+	      if(todo.edited) {
+	        return todo
+	      };
+	    })
+	    dataService.saveTodos(filteredTodos);
+	  };
+
+	}
+
+	module.exports = TodoCtrl;
+
 
 /***/ }
 ]);
