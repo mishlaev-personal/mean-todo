cebcb391e87d8897c3eb6b1a356fa846d0f31954
mishlaev-personal <mishlaev@gmail.com>
2016-10-02 18:59:36 +0300

    finished tutorial

--- public/scripts/todo.bundle.js
+++ public/scripts/todo.bundle.js
@@ -32,7 +32,7 @@ webpackJsonp([0],[
 
 	'use strict';
 
-	function DataService ($http) {
+	function DataService ($http, $q) {
 
 	  this.getTodos = function(cb) {
 	    $http.get('/api/todos').then(cb);
@@ -43,10 +43,25 @@ webpackJsonp([0],[
 	  };
 
 	  this.saveTodos = function(todos) {
-	    console.log("I saved " + todos.length + " todos!");
+	    var queue = [];
+	    todos.forEach(function(todo) {
+	      var request;
+	      if (!todo._id) {
+	        request = $http.post('/api/todos', todo);
+	      } else {
+	        request = $http.put('/api/todos/' + todo._id, todo).then(function(result) {
+	          todo = result.data.todo;
+	          return todo;
+	        });
+	      }
+	      queue.push(request);
+	    });
+	    return $q.all(queue).then(function(results) {
+	      console.log("I saved " + todos.length + " todos!");
+	    });
 	  };
 
-	}
+	};
 
 	module.exports = DataService;
 
@@ -133,10 +148,17 @@ webpackJsonp([0],[
 	        return todo
 	      };
 	    })
-	    dataService.saveTodos(filteredTodos);
+	    dataService.saveTodos(filteredTodos)
+	    .finally($scope.resetTodoState());
 	  };
 
-	}
+	  $scope.resetTodoState = function() {
+	    $scope.todos.forEach(function(todo) {
+	      todo.edited = false;
+	    });
+	  };
+
+	};
 
 	module.exports = TodoCtrl;
 
